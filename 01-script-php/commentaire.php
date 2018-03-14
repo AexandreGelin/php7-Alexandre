78382883f5f088fb943c6a0f1827a1f8eca2d2b9
Alexandre <alexandre.gelin71@gmail.com>
2018-03-14 11:13:19 +0100

    Cours

---
+++ 01-script-php/commentaire.php
@@ -0,0 +1,15 @@
+<?php
+
+
+print 'coucou \n'; // \n non interpreté
+print "coucou \n"; // \n interpreté
+
+/*
+    commentaire
+    mutiple
+    linge comme C et java
+*/
+
+# ne pas utilisr ce commentaire
+
+die ("c'est fini");
