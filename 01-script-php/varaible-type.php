5b106e9f069b3b2ba75ea2cab12d87c5bdac9f73
Alexandre <alexandre.gelin71@gmail.com>
2018-03-14 12:06:13 +0100

    toujours plus de cours

--- 01-script-php/varaible-type.php
+++ 01-script-php/varaible-type.php
@@ -1,3 +1,6 @@
+=======
+Type Scalaires
+=======
 <?php
 
 $captainName = "Morgan"; //type string
@@ -11,8 +14,41 @@ $captainStats =[
     'height' => $captainHeight,
     'is_dead' => $captainDead,
 ];
-
+//pour debugger contenu de variable
 var_dump($captainName);
 var_dump($captainAge);
 var_dump($captainHeight);
 var_dump($captainDead);
+
+
+// pour debug moins descriptif
+print_r($captainStats);
+
+//savoir de quel type est une variable
+var_dump(is_string($captainAge));
+var_dump(is_int($captainAge));
+var_dump(is_float($captainAge));
+var_dump(is_bool($captainAge));
+?>
+=====
+type Null
+=====
+<?php
+//variable non definie
+var_dump($nonDef);// Null
+var_dump(isset($captainAge)); // true
+var_dump(isset($nonDef)); //false
+print "c'est fini la \n";
+?>
+
+========
+LES CONSTANTES
+========
+<?php
+//ne peut pas etre modifiée
+define('PIRATE_FLAG', 'tete de mort');
+echo PIRATE_FLAG;
+
+
+
+ ?>
