e9f2945562d4ed11fe477025d14a745acacef21d
Alexandre <alexandre.gelin71@gmail.com>
2018-03-16 18:28:28 +0100

    avancée sur le challenge 1

--- challenge/01_la_fin_du_capitaine/la_fin_du_capitaine.php
+++ challenge/01_la_fin_du_capitaine/la_fin_du_capitaine.php
@@ -1,17 +1,21 @@
 <?php
 
+$captainAge = 23;
+$captainName = "Jacques Spareau";
+$captainIsDead = true;
+$captainLastWords = "AAaaargh...";
+
 $captain = [
-    'age' => 23,
-    'name' => 'Jacques Spareau',
-    'is_dead' => 'true',
-        'famous_last_words' => 'AAaaargh...',
+    'year' => $captainAge,
+    'name' => $captainName,
+    'is_dead' => $captainIsDead,
+    'famous_last_words' => $captainLastWords,
 ];
 
 
-
-if($captain['is_dead'] === true)
+if($captain['is_dead'] == true)
 {
-print 'Le capitaine '.$capitaine[name];
-print " est mort à l'age de ".$capitaine[year]." ans.\n";
-    'Ses derniers mots étaient "'.$capitaine[famous_last_words]."/ \n";
+print 'Le capitaine '.$captain[name];
+print " est mort à l'age de ".  $captain[year] . " ans.\n";
+print 'Ses derniers mots étaient "' . $captain[famous_last_words] . "/ \n";
 }
