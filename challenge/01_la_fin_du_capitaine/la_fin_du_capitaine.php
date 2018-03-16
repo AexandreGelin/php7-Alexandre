bb4b26e0d0af4be87a2ec8acf5c430ae3fbb76b0
Alexandre <alexandre.gelin71@gmail.com>
2018-03-16 18:29:47 +0100

    Fin du challenge n°1

--- challenge/01_la_fin_du_capitaine/la_fin_du_capitaine.php
+++ challenge/01_la_fin_du_capitaine/la_fin_du_capitaine.php
@@ -13,9 +13,9 @@ $captain = [
 ];
 
 
-if($captain['is_dead'] == true)
+if($captain['is_dead'] === true)
 {
-print 'Le capitaine '.$captain[name];
-print " est mort à l'age de ".  $captain[year] . " ans.\n";
-print 'Ses derniers mots étaient "' . $captain[famous_last_words] . "/ \n";
+print 'Le capitaine '.$captain['name'];
+print " est mort à l'age de ".  $captain['year'] . " ans.\n";
+print 'Ses derniers mots étaient "' . $captain['famous_last_words'] . "/ \n";
 }
